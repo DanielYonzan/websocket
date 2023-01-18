9fb2ba9d47a5104c06fb9a9916fd9dff7eef5db7
Daniel Tamang <daniel.tamang@f1soft.com>
2023-01-18 10:55:58 +0545

    Initial commit

---
+++ src/main/java/com/websocket/websocket/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.websocket.websocket.controller;
+
+import com.websocket.websocket.model.Greeting;
+import com.websocket.websocket.model.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+/**
+ * @author daniel <daniel.tamang@f1soft.com>
+ * created on 2023-01-17
+ */
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000);
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
---
+++ src/main/java/com/websocket/websocket/model/HelloMessage.java
@@ -0,0 +1,20 @@
+package com.websocket.websocket.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author daniel <daniel.tamang@f1soft.com>
+ * created on 2023-01-17
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloMessage {
+
+    private String name;
+
+}
